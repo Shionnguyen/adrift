8ef9d4445f729fb06bd911b8849cbf2488b9e0fb
nhi <nhigt@uci.edu>
2026-04-16 15:11:58 -0700

    sky scene
    implemented scene 0. player looks at sky with dialogue. must click space to proceed to main scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,22 +1,34 @@
 extends Node2D
 
-@onready var fishingUI = $FishingUI;
-@onready var fishingStatus = $FishStatus;
+@onready var fishingUI = $Game/FishingUI;
+@onready var fishingStatus = $Game/FishStatus;
 
-@onready var animations = $CharacterBody2D;
+@onready var animations = $Game/CharacterBody2D;
+
+# screens to display
+@onready var skyScene = $Sky;
+@onready var gameScene = $Game;
+
+var onSkyScene;
 
 # dialogue
 var dialogue_scene = preload("res://scenes/fish_dialogue.tscn");
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fishingUI.visible = false;
-	fishingStatus.visible = false;
-	fishingUI.fishingResults.connect(onFishingEnd);
-	animations.playerAnimate("idle", 0.3);
-	animations.reactionAnimate("waiting", 0.2)
+	gameScene.visible = false;
+	skyScene.visible = true;
+	onSkyScene = true;
+	playSkyScene();
 
 func _input(event: InputEvent) -> void:
+	if onSkyScene:
+		if event.is_action_pressed("ui_accept"): # go to next dialogue
+			onSkyScene = false;
+			skyScene.visible = false
+			gameStarts();
+		return;
+			
 	if event.is_action_pressed("start fishing"):
 		fishingUI.resetState();
 		fishingUI.visible = true;
@@ -43,3 +55,17 @@ func onFishingEnd(results):
 func showDialogue():
 	var dialogue_instance = dialogue_scene.instantiate();
 	add_child(dialogue_instance);
+
+func gameStarts():
+	gameScene.visible = true;
+	fishingUI.visible = false;
+	fishingStatus.visible = false;
+	fishingUI.fishingResults.connect(onFishingEnd);
+	animations.playerAnimate("idle", 0.3);
+	animations.reactionAnimate("waiting", 0.2);
+
+func playSkyScene():
+	# player will wake up looking at the sky
+	# dialogue appears  [ ... ]
+	$Sky/Dialogue/Label.text = "..."
+	$Sky/Dialogue.visible = true
